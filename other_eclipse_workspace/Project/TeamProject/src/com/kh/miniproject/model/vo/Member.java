10982ca799ac2fd6713f841581a76aa6f264fb9a
devHeon <gmlgjs11@naver.com>
2022-01-03 23:44:52 +0900

    ddd

--- other_eclipse_workspace/Project/TeamProject/src/com/kh/miniproject/model/vo/Member.java
+++ other_eclipse_workspace/Project/TeamProject/src/com/kh/miniproject/model/vo/Member.java
@@ -1,14 +1,8 @@
 package com.kh.miniproject.model.vo;
 
-import java.io.Serializable;
 
 // toString은 안 만들었어요
-public class Member extends Admin implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2378811559915259317L;
+public class Member extends Admin{
 
 	// 필드부
 	private int age;
